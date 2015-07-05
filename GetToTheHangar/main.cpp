d0e2ab55ecc72ccd5fdcb659d2311a9f0599beee
James Rees <James.rees@outlook.com>
2015-07-05 03:06:34 +0100

    v0.2.0: Added rudimentary timing system.

--- GetToTheHangar/main.cpp
+++ GetToTheHangar/main.cpp
@@ -1,24 +1,104 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
+sf::RenderWindow window;
+sf::CircleShape ball(50); //created a circle
+sf::CircleShape stupidBall(50);
+int movement = 1; // initialised movement, will occur in positive x direction.
+int stupidMovement = 1; //initialises movement for the non time stepped objects.
+
+void update(float dt);
+void render();
+void handleInput();
+
+
+
+int main(int, char const**)
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+    stupidBall.move(100, 0);
+    window.create(sf::VideoMode(800, 600), "Get To The Hangar!!!");
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+    
+    // sf::CircleShape ball(50);
+    ball.setFillColor(sf::Color::Red);
+    // Load a music to play
+    int dt = 1;
+    // Play the music
+    // music.play();
+    
+    // Start the game loop
+    
+    //  double currentTime = (double)
+    sf::Clock clock;
+    while (window.isOpen())
+    {
+        
+        sf::Time elapsed = clock.restart();
+        handleInput();
+        update(elapsed.asMilliseconds());
+        render();
+        
+    }
+    
+    return EXIT_SUCCESS;
+}
 
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
 
-	return 0;
+void update(float dt){
+    
+    if(ball.getPosition().y > 600){
+        movement = -1;
+        
+    }else if(ball.getPosition().y < 1){
+        movement = 1;
+    }
+    if(stupidBall.getPosition().y > 600){
+        stupidMovement = -1;
+        
+    }else if(stupidBall.getPosition().y < 1){
+        stupidMovement = 1;
+    }
+    
+    ball.move(0, movement * dt);
+    stupidBall.move(0,  stupidMovement );
+    std::cout << dt << std::endl;
+    
+    
+}
+
+void render(){
+    
+    
+    
+    // Clear screen
+    window.clear();
+    // Draw the sprite
+    //   window.draw(sprite);
+    window.draw(ball);
+    window.draw(stupidBall);
+    // Draw the string
+    //  window.draw(text);
+    
+    // Update the window
+    window.display();
+    
+}
+
+
+void handleInput(){
+    
+    
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        // Close window: exit
+        if (event.type == sf::Event::Closed) {
+            window.close();
+        }
+        
+        // Escape pressed: exit
+        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+            window.close();
+        }
+    }
 }
